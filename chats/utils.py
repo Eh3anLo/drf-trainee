3e98b9715490a560732ffc51ba488513e14f7a0a
ehsan <ehsunlotfy@gmail.com>
2024-07-23 14:39:30 +0330

    custom sort response

---
+++ chats/utils.py
@@ -0,0 +1,16 @@
+# Bubble sort
+def custom_sort(data):
+    data = list(data.items())
+    for n in range(len(data) - 1, 0, -1):
+        swapped = False
+        for i in range(n):
+            if int(data[i][0]) > int(data[i + 1][0]):
+                swapped = True
+                data[i], data[i + 1] = data[i + 1], data[i]
+        if not swapped:
+            continue
+    return dict(data)
+
+
+
+    
--- chats/views.py
+++ chats/views.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import json
-import os
+
+from .utils import custom_sort
+
 class MessageView(APIView):
 
     def get(self , request , filename="data.json"):
-        print(filename)
         with open(filename , "r") as response_file:
             respones_json = json.load(response_file)
+        respones_json = custom_sort(respones_json)
         return Response(respones_json)
+    
+    
 
     # def get(self , request):
     #     with open("data.json" , "r") as response_file:
