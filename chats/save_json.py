35f428fa587e960f05907404386896d9fa9e2ea6
Eh3anLo <ehsunlotfy@gmail.com>
2024-07-24 13:15:30 +0330

    raise exception on un validated data

--- chats/save_json.py
+++ chats/save_json.py
@@ -8,13 +8,10 @@ def save_json_from_file(filename):
         for value in data_post.values():
             for data in value:
                 cleaned_data.append(data)
-    serializer = ChatSerializer(data=cleaned_data , many=True)
+    serializer = ChatSerializer(data={'heel' : 'noe'})
     serializer.is_valid(raise_exception=True)
-    if serializer.validated_data:
-        serializer.save()
-        print("Save from file completed ...")
-        return
-    return serializer.data
+    serializer.save()
+    print("Save from file completed ...")
     
 
     
